c2f01fdb4c3ea0d2478716a339240a00f9e33939
moo1o <moody@dunamu.com>
2024-01-05 18:06:28 +0900

    first commit

---
+++ src/public/js/app.js
@@ -0,0 +1,28 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form")
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+function handleOpen(){
+    console.log("Connected to Server ✅");
+}
+socket.addEventListener("open", handleOpen);
+
+socket.addEventListener("message", (message) => {
+    console.log("New Message : ", message.data);
+});
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server ❌");
+});
+
+setTimeout(() => {
+    socket.send("hello from the browser!");
+}, 3000);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(input.value);
+}
+
+messageForm.addEventListener("submit", handleSubmit);
---
+++ src/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import http from "http";
+import WebSocket from "ws";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+
+function onSocketClose(){
+    console.log("Disconnect from the Browser ❌")
+}
+
+const sockets = [];
+
+
+wss.on("connection", (socket) => {
+    console.log("Connected to the Browser ✅");
+    sockets.push(socket);
+    socket.on("close", onSocketClose);
+    socket.on("message", (message) => {
+        sockets.forEach((aSocket) => {
+            aSocket.send(message.toString('utf8'));
+        });
+    });
+    socket.send("hello!!!!");
+});
+
+
+server.listen(3000, handleListen);
